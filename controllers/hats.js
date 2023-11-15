b0045f046200c4a3f6770817be3762ccc45f5370
prudhvi <prudhvimohan8@gmail.com>
2023-11-15 17:21:47 -0600

    CRUD operations

--- controllers/hats.js
+++ controllers/hats.js
@@ -36,24 +36,32 @@ exports.hats_create_post = async function (req, res) {
     }
 };
 // Handle hats delete form on DELETE.
-exports.hats_delete = function (req, res) {
-    res.send('NOT IMPLEMENTED: hats delete DELETE ' + req.params.id);
+exports.hats_delete = async function (req, res) {
+    console.log("delete " + req.params.id)
+    try {
+        result = await hats.findByIdAndDelete(req.params.id)
+        console.log("Removed " + result)
+        res.send(result)
+    } catch (err) {
+        res.status(500)
+        res.send(`{"error": Error deleting ${err}}`);
+    }
 };
 // Handle hats update form on PUT.
 exports.hats_update_put = async function (req, res) {
     console.log(`update on id ${req.params.id} with body
 ${JSON.stringify(req.body)}`)
-try {
-let toUpdate = await hats.findById( req.params.id)
-// Do updates of properties
-if(req.body.price)
-toUpdate.price= req.body.price;
-if(req.body.style) toUpdate.style = req.body.style;
-if(req.body.color) toUpdate.color = req.body.color;
-let result = await toUpdate.save();
-console.log("Sucess " + result)
-res.send(result)
-}  catch (err) {
+    try {
+        let toUpdate = await hats.findById(req.params.id)
+        // Do updates of properties
+        if (req.body.price)
+            toUpdate.price = req.body.price;
+        if (req.body.style) toUpdate.style = req.body.style;
+        if (req.body.color) toUpdate.color = req.body.color;
+        let result = await toUpdate.save();
+        console.log("Sucess " + result)
+        res.send(result)
+    } catch (err) {
         res.status(500)
         res.send(`{"error": ${err}: Update for id ${req.params.id}
     failed`);
@@ -84,3 +92,50 @@ exports.hats_view_all_Page = async function (req, res) {
     }
 };
 
+exports.hats_view_one_Page = async function (req, res) {
+    console.log("single view for id " + req.query.id)
+    try {
+        result = await hats.findById(req.query.id)
+        res.render('hatsdetail',
+            { title: 'hats Detail', toShow: result });
+    }
+    catch (err) {
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+    }
+};
+
+
+exports.hats_create_Page = function (req, res) {
+    console.log("create view")
+    try {
+        res.render('hatscreate', { title: 'hats Create' });
+    }
+    catch (err) {
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+    }
+}
+exports.hats_update_Page = async function (req, res) {
+    console.log("update view for item " + req.query.id)
+    try {
+        let result = await hats.findById(req.query.id)
+        res.render('hatsupdate', { title: 'hats Update', toShow: result });
+    }
+    catch (err) {
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+    }
+};
+
+exports.hats_delete_Page = async function (req, res) {
+    console.log("Delete view for id " + req.query.id)
+    try {
+        result = await hats.findById(req.query.id)
+        res.render('hatsdelete', {title: 'hats Delete', toShow:result});
+    }
+    catch (err) {
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+    }
+};
--- routes/hats.js
+++ routes/hats.js
@@ -3,4 +3,8 @@ const hats_controlers= require('../controllers/hats');
 var router = express.Router();
 /* GET hats */
 router.get('/', hats_controlers.hats_view_all_Page);
+router.get('/detail', hats_controlers.hats_view_one_Page);
+router.get('/create', hats_controlers.hats_create_Page);
+router.get('/update', hats_controlers.hats_update_Page);
+router.get('/delete', hats_controlers.hats_delete_Page);
 module.exports = router;
