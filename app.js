55e7a31907d74fdccfac0cd6baaa22f85b8b9972
prudhvi <prudhvimohan8@gmail.com>
2023-11-26 16:41:02 -0600

    added login screen

--- app.js
+++ app.js
@@ -6,24 +6,24 @@ var logger = require('morgan');
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 
-require('dotenv').config();
-const connectionString = process.env.MONGO_CON
-mongoose = require('mongoose');
-mongoose.connect(connectionString);
-const Schema = mongoose.Schema;
-const passportLocalMongoose = require("passport-local-mongoose");
-const accountSchema = new Schema({
-  username: String,
-  password: String
-});
-accountSchema.plugin(passportLocalMongoose);
-
-var db = mongoose.connection;
-//Bind connection to error event
-db.on('error', console.error.bind(console, 'MongoDB connectionerror:'));
-db.once("open", function () {
-  console.log("Connection to DB succeeded")
-});
+passport.use(new LocalStrategy(
+  function (username, password, done) {
+    Account.findOne({ username: username })
+      .then(function (user) {
+        if (err) { return done(err); }
+        if (!user) {
+          return done(null, false, { message: 'Incorrect username.' });
+        }
+        if (!user.validPassword(password)) {
+          return done(null, false, { message: 'Incorrect password.' });
+        }
+        return done(null, user);
+      })
+      .catch(function (err) {
+        return done(err)
+      })
+  })
+)
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -32,6 +32,46 @@ var boardRouter = require('./routes/board');
 var chooseRouter = require('./routes/choose');
 var hats = require('./models/hats');
 var resourceRouter = require('./routes/resource');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(require('express-session')({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/hats', hatsRouter)
+app.use('/board', boardRouter)
+app.use('/choose', chooseRouter)
+app.use('/resource', resourceRouter)
+require('dotenv').config();
+const connectionString = process.env.MONGO_CON
+mongoose = require('mongoose');
+mongoose.connect(connectionString);
+
+
+var db = mongoose.connection;
+//Bind connection to error event
+db.on('error', console.error.bind(console, 'MongoDB connectionerror:'));
+db.once("open", function () {
+  console.log("Connection to DB succeeded")
+});
+
+
 
 async function recreateDB() {
   // Delete everything
@@ -64,31 +104,15 @@ if (reseed) {
   recreateDB();
 }
 
-var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/hats', hatsRouter)
-app.use('/board', boardRouter)
-app.use('/choose', chooseRouter)
-app.use('/resource', resourceRouter)
-app.use(require('express-session')({
-  secret: 'keyboard cat',
-  resave: false,
-  saveUninitialized: false
-}));
-app.use(passport.initialize());
-app.use(passport.session());
+
+var Account = require("./models/account");
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
+
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
@@ -105,22 +129,5 @@ app.use(function (err, req, res, next) {
   res.render('error');
 });
 
-passport.use(new LocalStrategy(
-  function (username, password, done) {
-    Account.findOne({ username: username })
-      .then(function (user) {
-        if (err) { return done(err); }
-        if (!user) {
-          return done(null, false, { message: 'Incorrect username.' });
-        }
-        if (!user.validPassword(password)) {
-          return done(null, false, { message: 'Incorrect password.' });
-        }
-        return done(null, user);
-      })
-      .catch(function (err) {
-        return done(err)
-      })
-  })
-)
+
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,82 @@
 var express = require('express');
+var passport = require('passport');
 var router = express.Router();
+var Account = require('../models/account');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+// Home page
+router.get('/', function (req, res) {
+  res.render('index', { title: 'Hats App', user: req.user });
+});
+
+// Registration page
+router.get('/register', function(req, res) {
+  res.render('register', { title: 'Hats App Registration' });
+});
+
+// Registration POST route
+router.post('/register', function(req, res) {
+  Account.findOne({ username: req.body.username })
+    .then(function (user) {
+      if (user != null) {
+        console.log("User already exists: " + req.body.username);
+        return res.render('register', {
+          title: 'Registration',
+          message: 'Existing User',
+          account: req.body.username
+        });
+      }
+
+      let newAccount = new Account({ username: req.body.username });
+      Account.register(newAccount, req.body.password, function(err, user) {
+        if (err) {
+          console.log("Database creation issue: " + err);
+          return res.render('register', {
+            title: 'Registration',
+            message: 'Database access error',
+            account: req.body.username
+          });
+        }
+
+        if (!user) {
+          return res.render('register', {
+            title: 'Registration',
+            message: 'Database access error',
+            account: req.body.username
+          });
+        }
+
+        console.log('Success, redirect');
+        res.redirect('/');
+      });
+    })
+    .catch(function (err) {
+      return res.render('register', {
+        title: 'Registration',
+        message: 'Registration error',
+        account: req.body.username
+      });
+    });
+});
+
+// Login page
+router.get('/login', function(req, res) {
+  res.render('login', { title: 'Hats App Login', user: req.user });
+});
+
+// Login POST route
+router.post("/login", passport.authenticate("local"), function (req, res) {
+  res.redirect("/");
+});
+
+// Logout route
+router.get('/logout', function(req, res) {
+  req.logout();
+  res.redirect('/');
+});
+
+// Ping route
+router.get('/ping', function(req, res) {
+  res.status(200).send('pong');
 });
 
 module.exports = router;
