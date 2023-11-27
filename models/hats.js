ba43674188c27ff56d264785438766a217831213
prudhvi <prudhvimohan8@gmail.com>
2023-11-27 09:37:56 -0600

    7 and bouns

--- models/hats.js
+++ models/hats.js
@@ -1,9 +1,20 @@
 const { Double } = require("mongodb")
 const mongoose = require("mongoose")
 const hatsSchema = mongoose.Schema({
-style: String,
-color: String,
-price:Number,
+style: {
+    type:String,
+    require:true,
+    maxlength:25
+},
+color: {
+    type:String,
+    require:true,
+    maxlength:25
+},
+price:{type:Number,
+    min:5,
+    max:1000
+},
 })
 module.exports = mongoose.model("hats",hatsSchema)
 
--- routes/hats.js
+++ routes/hats.js
@@ -1,10 +1,19 @@
 var express = require('express');
-const hats_controlers= require('../controllers/hats');
+const hats_controlers = require('../controllers/hats');
 var router = express.Router();
+
+// redirect to login.
+const secured = (req, res, next) => {
+    if (req.user) {
+        return next();
+    }
+    res.redirect("/login");
+}
+
 /* GET hats */
 router.get('/', hats_controlers.hats_view_all_Page);
 router.get('/detail', hats_controlers.hats_view_one_Page);
-router.get('/create', hats_controlers.hats_create_Page);
-router.get('/update', hats_controlers.hats_update_Page);
-router.get('/delete', hats_controlers.hats_delete_Page);
+router.get('/create', secured,hats_controlers.hats_create_Page);
+router.get('/update', secured, hats_controlers.hats_update_Page);
+router.get('/delete', secured,hats_controlers.hats_delete_Page);
 module.exports = router;
